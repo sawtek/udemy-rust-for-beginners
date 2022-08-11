36524ac347e5af04036c6416c7244561f09a029e
S. Alex Wilson <sawilson2009@gmail.com>
2022-08-11 17:31:36 -0400

    activity 7 printing enums

--- src/bin/a7.rs
+++ src/bin/a7.rs
@@ -10,4 +10,25 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+enum Color {
+    Cyan,
+    Magenta, // Not used on purpose, to check out the compiler warning
+    Yellow,
+    Black,
+}
+
+fn print_color_name(color: Color) {
+    let color_name = match color {
+        Color::Cyan => "Cyan",
+        Color::Magenta => "Magenta",
+        Color::Yellow => "Yellow",
+        Color::Black => "Black",
+    };
+    println!("{color_name}");
+}
+
+fn main() {
+    print_color_name(Color::Black);
+    print_color_name(Color::Yellow);
+    print_color_name(Color::Cyan);
+}
