5f4b99a86d93cd4cbeefdd8ced0ad8554ddddb3f
S. Alex Wilson <sawilson2009@gmail.com>
2022-08-11 17:44:41 -0400

    activity 8, structs

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -9,4 +9,39 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum Flavor {
+    Orange,
+    Cherry,
+}
+
+struct Drink {
+    flavor: Flavor,
+    ounces: i32,
+}
+
+fn print_drink(drink: Drink) {
+    println!(
+        "{} flavor",
+        match drink.flavor {
+            // ok cool the language lets people do this...
+            // notable that if i use a ?: here it prints the string with quotes surrounding it
+            Flavor::Orange => "orange",
+            Flavor::Cherry => "cherry",
+        }
+    );
+    println!("{} fl oz", drink.ounces);
+}
+
+fn main() {
+    let my_drink = Drink {
+        flavor: Flavor::Cherry,
+        ounces: 173,
+    };
+    print_drink(my_drink);
+
+    print_drink(Drink {
+        // ok cool the language lets people do this
+        flavor: Flavor::Orange,
+        ounces: 8,
+    });
+}
