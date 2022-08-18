6adaa0a8e9fd5b96646c6401859854c55d0db692
S. Alex Wilson <sawilson2009@gmail.com>
2022-08-18 18:56:17 -0400

    activity 13 - vectors

--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -10,4 +10,20 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    let vector = vec![10, 20, 30, 40];
+    for entry in &vector {
+        if *entry == 30 {
+            // hmm, need a star here
+            println!("thirty");
+        } else {
+            println!("{}", entry);
+        }
+        match entry {
+            // but not here?
+            30 => println!("thirty"),
+            _ => println!("{}", entry), // or here?
+        }
+    }
+    println!("{}", vector.len());
+}
