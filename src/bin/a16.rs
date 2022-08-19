4ea02e769ed88447ccc41c0e6ec6b40e67b0345a
S. Alex Wilson <sawilson2009@gmail.com>
2022-08-18 20:00:53 -0400

    activity 16 - optional members

--- src/bin/a16.rs
+++ src/bin/a16.rs
@@ -11,4 +11,28 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+struct Student {
+    name: String,
+    locker: Option<i32>,
+}
+
+fn main() {
+    let students = vec![
+        Student {
+            name: "Alice".to_owned(),
+            locker: None,
+        },
+        Student {
+            name: "Bob".to_owned(),
+            locker: Some(120),
+        },
+    ];
+
+    for student in &students {
+        print!("Name {:?} ", student.name);
+        match student.locker {
+            Some(locker) => println!("locker {:?}", locker),
+            None => println!("no locker"),
+        }
+    }
+}
