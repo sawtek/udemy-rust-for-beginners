d431a3f8009837531c6f84583dbf96d53b464edd
S. Alex Wilson <sawilson2009@gmail.com>
2022-08-22 19:38:24 -0400

    a18b - the question mark operator

--- src/bin/a18b.rs
+++ src/bin/a18b.rs
@@ -22,4 +22,67 @@
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
 
-fn main() {}
+enum EmployeeRole {
+    Maintenance,
+    Marketing,
+    Manager,
+    LineSupervisor,
+    Kitchen,
+    AssemblyTech,
+}
+
+struct Employee {
+    role: EmployeeRole,
+    terminated: bool,
+}
+
+impl Employee {
+    fn access(&self) -> Result<(), String> {
+        if !self.terminated {
+            match self.role {
+                EmployeeRole::Maintenance => Ok(()),
+                EmployeeRole::Marketing => Ok(()),
+                EmployeeRole::Manager => Ok(()),
+                _ => Err("role has no permission".to_owned()),
+            }
+        } else {
+            Err("terminated".to_owned())
+        }
+    }
+}
+
+fn print_access(employee: &Employee) -> Result<(), String> {
+    employee.access()?;
+    println!("granted");
+    Ok(())
+}
+
+fn main() {
+    let emp1 = Employee {
+        role: EmployeeRole::Maintenance,
+        terminated: false,
+    };
+
+    let emp2 = Employee {
+        role: EmployeeRole::Maintenance,
+        terminated: true,
+    };
+
+    let emp3 = Employee {
+        role: EmployeeRole::AssemblyTech,
+        terminated: false,
+    };
+
+    match print_access(&emp1) {
+        Err(msg) => println!("denied, {:?}", msg),
+        _ => (),
+    }
+    match print_access(&emp2) {
+        Err(msg) => println!("denied, {:?}", msg),
+        _ => (),
+    }
+    match print_access(&emp3) {
+        Err(msg) => println!("denied, {:?}", msg),
+        _ => (),
+    }
+}
