247ad64a91999a6c8d4c46274be4334d8a8b2ef5
S. Alex Wilson <sawilson2009@gmail.com>
2022-08-22 22:23:16 -0400

    a22 - testing

--- src/bin/a22.rs
+++ src/bin/a22.rs
@@ -23,12 +23,63 @@ fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
 
 /// Divides a and b.
 fn div(a: i32, b: i32) -> Option<i32> {
-    Some(a / b)
+    if b == 0 {
+        None
+    } else {
+        Some(a / b)
+    }
 }
 
 /// Takes two strings and places them immediately one after another.
 fn concat(first: &str, second: &str) -> String {
-    format!("{} {}", first, second)
+    format!("{}{}", first, second)
 }
 
 fn main() {}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn clamp_low() {
+        let actual = clamp(1, 10, 20);
+        let expected = 10;
+        assert_eq!(actual, expected, "wrong low clamp");
+    }
+
+    #[test]
+    fn clamp_hi() {
+        let actual = clamp(100, 10, 20);
+        let expected = 20;
+        assert_eq!(actual, expected, "wrong hi clamp");
+    }
+
+    #[test]
+    fn clamp_no() {
+        let actual = clamp(15, 10, 20);
+        let expected = 15;
+        assert_eq!(actual, expected, "wrong no change clamp");
+    }
+
+    #[test]
+    fn div_test() {
+        let actual = div(10, 5);
+        let expected = Some(2);
+        assert_eq!(actual, expected, "wrong division");
+    }
+
+    #[test]
+    fn div_zero() {
+        let actual = div(10, 0);
+        let expected = None;
+        assert_eq!(actual, expected, "wrong division by zero");
+    }
+
+    #[test]
+    fn concat_test() {
+        let actual = concat("ab", "cd");
+        let expected = "abcd".to_owned();
+        assert_eq!(actual, expected, "wrong concatenation");
+    }
+}
