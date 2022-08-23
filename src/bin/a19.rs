ccca576fa4f074761007e31fb2c5b63465960b0c
S. Alex Wilson <sawilson2009@gmail.com>
2022-08-22 20:35:56 -0400

    activity 19 -using hashmaps

--- src/bin/a19.rs
+++ src/bin/a19.rs
@@ -13,4 +13,32 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+enum ItemType {
+    Chair,
+    Bed,
+    Table,
+    Couch,
+}
+
+fn main() {
+    let mut items: HashMap<ItemType, i32> = HashMap::new();
+    items.insert(ItemType::Chair, 5);
+    items.insert(ItemType::Bed, 3);
+    items.insert(ItemType::Table, 2);
+    items.insert(ItemType::Couch, 0);
+
+    let mut sum: i32 = 0;
+
+    for (item_type, item_count) in items.iter() {
+        let msg = match item_count {
+            0 => "out of stock".to_owned(),
+            _ => format!("{:?}", item_count),
+        };
+        println!("{:?} {:?}", item_type, msg);
+        sum += item_count;
+    }
+    println!("{:?} items in stock", sum);
+}
